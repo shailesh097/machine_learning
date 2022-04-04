8baf3e45058c49bfe66e35b12962d03ecc9705dd
shailesh097 <sailesh.pokharel.234@gmail.com>
2022-04-04 19:02:12 +0545

    commit

--- ML using Scikit-learn/first_ml.py
+++ ML using Scikit-learn/first_ml.py
@@ -1,7 +1,8 @@
 """
-A machine learning model takes features as input and gives label as output.
-For example, a machine learning model is given a picture of a cat. By looking at it's features
-the model has to determine if it is a cat or not. A cat has unique set of ears which the ML model
-already knows. So the model gives the ears as input which is also called a feature in ML term; by looking at
-the feature of cat's ear then the ML model lables the given picture as a cat.
+A machine learning model takes features as input and gives label as output.  For example, a machine learning model is given a picture of a cat. 
+By looking at it's features the model has to determine if it is a cat or not. A cat has unique set of ears which the ML model already knows. 
+So the model gives the ears as input which is also called a feature in ML term; 
+by looking at the feature of cat's ear then the ML model lables the given picture as a cat.
 """
+
+import sklearn
